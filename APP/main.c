1d18af62fced072c403bc532b9dc8194556b3adc
marwan basem <marob27105@gmail.com>
2026-03-10 17:31:27 +0200

    implement task 1

--- APP/main.c
+++ APP/main.c
@@ -3,25 +3,43 @@
 
 
 */
+#include "../HAL/switch/switch_interface.h"
 #include "../HAL/LED/LED_interface.h"
 #include "../MCAL/GPIO/GPIO_interface.h"
 
-void delay(void)
+void delay(u8 s)
 {
-    unsigned int i;
-    for(i = 0; i < 50000; i++);
+  unsigned int i;
+  unsigned int time = s * 10000;
+  for(i = 0; i < time; i++);
 }
 
 void main()
 {
-    LED_Init(GPIO_PORTB, GPIO_PIN0);
+  LED_Init(GPIO_PORTB, GPIO_PIN0);
+  LED_Init(GPIO_PORTB, GPIO_PIN7);
+  switch_Init(GPIO_PORTA, GPIO_PIN0);
+  switch_Init(GPIO_PORTA, GPIO_PIN5);
 
-    while(1)
-    {
-        LED_On(GPIO_PORTB, GPIO_PIN0);
-        delay();
-
-        LED_Off(GPIO_PORTB, GPIO_PIN0);
-        delay();
+  while(1)
+  {
+    if (get_switch_state(GPIO_PORTA, GPIO_PIN0)){
+      LED_On(GPIO_PORTB, GPIO_PIN0);
+      LED_On(GPIO_PORTB, GPIO_PIN7);
+      delay(2);
+      LED_Off(GPIO_PORTB, GPIO_PIN0);
+      LED_Off(GPIO_PORTB, GPIO_PIN7);
+      delay(2);
+    } else if (get_switch_state(GPIO_PORTA, GPIO_PIN5)) {
+      LED_On(GPIO_PORTB, GPIO_PIN0);
+      LED_On(GPIO_PORTB, GPIO_PIN7);
+      delay(5);
+      LED_Off(GPIO_PORTB, GPIO_PIN0);
+      LED_Off(GPIO_PORTB, GPIO_PIN7);
+      delay(5);
+    } else {
+      LED_Off(GPIO_PORTB, GPIO_PIN0);
+      LED_Off(GPIO_PORTB, GPIO_PIN7);
     }
+  }
 }
